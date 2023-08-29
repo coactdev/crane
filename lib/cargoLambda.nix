d44daa4a6a3537b53083ccc9d871abe9b55941ca
Paul Liljenberg <liljenberg.paul@gmail.com>
2023-08-29 12:24:19 +0200

    allow introspect

--- lib/cargoLambda.nix
+++ lib/cargoLambda.nix
@@ -19,11 +19,5 @@ mkCargoDerivation (args // {
 
   pnameSuffix = "-lambda";
 
-  # Avoid trying to introspect the Cargo.toml file as it won't exist in the
-  # filtered source (it also might not exist in the original source either).
-  # So just use some placeholders here in case the caller did not set them.
-  pname = args.pname or "crate";
-  version = args.version or "0.0.0";
-
   nativeBuildInputs = (args.nativeBuildInputs or [ ]) ++ [ cargo-lambda ];
 })
